e747b8f8d0e0cf3211fa5d20ce6edd879cd860e4
wujiajian1998 <jiajian_wu1998@126.com>
2023-02-24 21:15:46 +0800

    first version

---
+++ main.cpp
@@ -0,0 +1,235 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/epoll.h>
+#include "locker.h"
+#include "threadpool.h"
+#include "http_conn.h"
+#include "noactive/lst_timer.h"
+#define MAX_FD 65536   // 最大的文件描述符个数
+#define MAX_EVENT_NUMBER 10000  // 监听的最大的事件数量
+#define TIMESLOT 5
+
+
+static int pipefd[2];
+static sort_timer_lst timer_lst;
+static int epollfd = 0;
+
+
+// 添加文件描述符
+extern void addfd( int epollfd, int fd, bool one_shot );
+extern void removefd( int epollfd, int fd );
+extern int setnonblocking(int fd);
+void sig_handler( int sig )
+{
+    int save_errno = errno;
+    int msg = sig;
+    send( pipefd[1], ( char* )&msg, 1, 0 );
+    errno = save_errno;
+}
+
+void addsig(int sig, void( handler )(int)){
+    struct sigaction sa;
+    memset( &sa, '\0', sizeof( sa ) );
+    sa.sa_handler = handler;
+    sigfillset( &sa.sa_mask );
+    assert( sigaction( sig, &sa, NULL ) != -1 );
+}
+
+void addsig( int sig )
+{
+    struct sigaction sa;
+    memset( &sa, '\0', sizeof( sa ) );
+    sa.sa_handler = sig_handler;
+    sa.sa_flags |= SA_RESTART;
+    sigfillset( &sa.sa_mask );
+    assert( sigaction( sig, &sa, NULL ) != -1 );
+}
+
+void timer_handler()
+{
+    // 定时处理任务，实际上就是调用tick()函数
+    timer_lst.tick();
+    // 因为一次 alarm 调用只会引起一次SIGALARM 信号，所以我们要重新定时，以不断触发 SIGALARM信号。
+    alarm(TIMESLOT);
+}
+
+// 定时器回调函数，它删除非活动连接socket上的注册事件，并关闭之。
+void cb_func(client_data* user_data )
+{
+    epoll_ctl( epollfd, EPOLL_CTL_DEL, user_data->sockfd, 0 );
+    assert( user_data );
+    close( user_data->sockfd );
+    printf( "close fd %d\n", user_data->sockfd );
+}
+
+int main( int argc, char* argv[] ) {
+    
+    if( argc <= 1 ) {
+        printf( "usage: %s port_number\n", basename(argv[0]));
+        return 1;
+    }
+
+    int port = atoi( argv[1] );
+    addsig( SIGPIPE, SIG_IGN );
+
+    threadpool< http_conn >* pool = NULL;
+    try {
+        pool = new threadpool<http_conn>;
+    } catch( ... ) {
+        return 1;
+    }
+
+    http_conn* users = new http_conn[ MAX_FD ];
+
+    int listenfd = socket( PF_INET, SOCK_STREAM, 0 );
+
+    int ret = 0;
+    struct sockaddr_in address;
+    bzero( &address, sizeof( address ) );
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_family = AF_INET;
+    address.sin_port = htons( port );
+
+    // 端口复用
+    int reuse = 1;
+    setsockopt( listenfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof( reuse ) );
+    ret = bind( listenfd, ( struct sockaddr* )&address, sizeof( address ) );
+    ret = listen( listenfd, 5 );
+
+    // 创建epoll对象，和事件数组，添加
+    epoll_event events[ MAX_EVENT_NUMBER ];
+    int epollfd = epoll_create( 5 );
+    // 添加到epoll对象中
+    addfd( epollfd, listenfd, false);
+    http_conn::m_epollfd = epollfd;
+
+    // 创建管道
+    ret = socketpair(PF_UNIX, SOCK_STREAM, 0, pipefd);
+    assert( ret != -1 );
+    setnonblocking( pipefd[1] );
+    addfd( epollfd, pipefd[0] , false);
+
+    // 设置信号处理函数
+    addsig( SIGALRM );
+    addsig( SIGTERM );
+    bool stop_server = false;
+
+    client_data* users_timer = new client_data[MAX_FD]; 
+    bool timeout = false;
+    alarm(TIMESLOT);  // 定时,5秒后产生SIGALARM信号
+
+    while(!stop_server) {
+        
+        int number = epoll_wait( epollfd, events, MAX_EVENT_NUMBER, -1 );
+        
+        if ( ( number < 0 ) && ( errno != EINTR ) ) {
+            printf( "epoll failure\n" );
+            break;
+        }
+
+        for ( int i = 0; i < number; i++ ) {
+            int sockfd = events[i].data.fd;
+            if( sockfd == listenfd ) {
+                //监听到的是主线程
+                struct sockaddr_in client_address;
+                socklen_t client_addrlength = sizeof( client_address );
+                int connfd = accept( listenfd, ( struct sockaddr* )&client_address, &client_addrlength );
+                
+                if ( connfd < 0 ) {
+                    printf( "errno is: %d\n", errno );
+                    continue;
+                } 
+
+                if( http_conn::m_user_count >= MAX_FD ) {
+                    close(connfd);
+                    continue;
+                }
+                users[connfd].init( connfd, client_address);
+                //初始化client_data数据
+                //创建定时器，设置回调函数和超时时间，绑定用户数据，将定时器添加到链表中
+                users_timer[connfd].address = client_address;
+                users_timer[connfd].sockfd = connfd;
+                util_timer* timer = new util_timer;
+                timer->user_data = &users_timer[connfd];
+                timer->cb_func = cb_func;
+                time_t cur = time( NULL );
+                timer->expire = cur + 3 * TIMESLOT;
+                users_timer[connfd].timer = timer;
+                timer_lst.add_timer( timer );
+
+            } else if( events[i].events & ( EPOLLRDHUP | EPOLLHUP | EPOLLERR ) ) {
+                //发生异常关闭连接
+                users[sockfd].close_conn();
+
+            } else if( ( sockfd == pipefd[0] ) && ( events[i].events & EPOLLIN ) ){
+
+                // 处理信号
+                int sig;
+                char signals[1024];
+                ret = recv( pipefd[0], signals, sizeof( signals ), 0 );
+                if( ret == -1 ) {
+                    continue;
+                } else if( ret == 0 ) {
+                    continue;
+                } else  {
+                    for( int i = 0; i < ret; ++i ) {
+                        switch( signals[i] )  {
+                            case SIGALRM:
+                            {
+                                // 用timeout变量标记有定时任务需要处理，但不立即处理定时任务
+                                // 这是因为定时任务的优先级不是很高，我们优先处理其他更重要的任务。
+                                timeout = true;
+                                break;
+                            }
+                            case SIGTERM:
+                            {
+                                stop_server = true;
+                            }
+                        }
+                    }
+                }
+
+
+            } else if(events[i].events & EPOLLIN) {
+                //请求读事件
+                if(users[sockfd].read()) {
+                    //加入线程池任务队列
+                    pool->append(users + sockfd);
+                } else {
+                    //一次性写失败 关闭连接
+                    users[sockfd].close_conn();
+                }
+
+            } else if( events[i].events & EPOLLOUT ) {
+
+                if( !users[sockfd].write() ) {
+                    //一次性写失败 关闭连接
+                    users[sockfd].close_conn();
+                }
+
+            }
+            else 
+            {
+
+            }
+        }
+        // 最后处理定时事件，因为I/O事件有更高的优先级。当然，这样做将导致定时任务不能精准的按照预定的时间执行。
+        if( timeout ) {
+            timer_handler();
+            timeout = false;
+        }
+    }
+    
+    close( epollfd );
+    close( listenfd );
+    delete [] users;
+    delete pool;
+    return 0;
+}
